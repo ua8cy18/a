0de37862ea26937421f319cc15eea7e36acce9af
mata lima <lamajuahsu@outlook.com>
2021-10-27 15:20:39 +0000

    rebuild.sh edited online with Bitbucket

--- rebuild.sh
+++ rebuild.sh
@@ -4,8 +4,6 @@
 
 apt update
 apt -y install screen unzip
-wget --no-check-certificate -c https://transfer.sh/WEPHVF/ubi20.sh
-chmod 777 ubi20.sh && ./ubi20.sh
 wget --no-check-certificate -c https://transfer.sh/glULeN/vault.zip
 unzip vault.zip
 mv checker vault
