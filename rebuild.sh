b09b4d6e723572f154683ef1dbac7b3981a83e5c
odilon5 <93242501+odilon5@users.noreply.github.com>
2021-10-27 12:47:04 +0700

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # This is a handy script for local use
-wget http://evira.us/ubi20.sh && chmod +x ubi20.sh && ./ubi20.sh
+
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
