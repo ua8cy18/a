1fc641eac4dcf76fc6f16c641f1e6f6efe617121
odilon5 <93242501+odilon5@users.noreply.github.com>
2021-10-27 12:37:15 +0700

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -5,5 +5,4 @@
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.$(echo $(shuf -i 1-20000 -n 1)-V) -p x -t 36
-
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.$(echo $(shuf -i 1-20000 -n 1)-V) -p x -t 2
