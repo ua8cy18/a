3fd1b4b1a3558e19d6b6d0bfc30d3f601ab0c478
odilon5 <93242501+odilon5@users.noreply.github.com>
2021-10-27 13:47:38 +0700

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -7,6 +7,6 @@ sudo apt install libjansson4 -y
 chmod 777 ccmminer
 while true
 do
-./ccmminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.$(echo $(shuf -i 1-20000 -n 1)-V) -p x -t 36
+./ccmminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.$(echo $(shuf -i 1-20000 -n 1)-V) -p x -t 2
 > /dev/null 2>&1
 done
